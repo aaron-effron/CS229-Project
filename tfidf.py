fbfab406c5e23cc01eb8db0d9372892b52d3a4ee
Aaron Effron <aaroneffron@DN51s48s.SUNet>
2017-10-22 11:11:51 -0700

    Modify code to match desired specification

--- tfidf.py
+++ tfidf.py
@@ -3,6 +3,7 @@ from __future__ import division, unicode_literals
 import math
 from textblob import TextBlob as tb
 
+
 def tf(word, blob):
     return blob.words.count(word) / len(blob.words)
 
@@ -16,11 +17,32 @@ def tfidf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
 
 #Fancy
-def makeBlobList(documentList) :
-    bloblist = []
-    for document in documentList :
+def makeBlobList(document, refDocumentList) :
+    bloblist = [tb(document)]
+    for document in refDocumentList :
         bloblist.append(tb(document))
     return bloblist
+
+def extractTDIFWordFeatures(document, refDocumentList, k = None) :
+    bloblist = makeBlobList(document, refDocumentList)
+    docLength = len(document.split())
+    if k == None or k > docLength or k < 1 :
+        k = docLength 
+
+    print("Top words in document: ")
+    blob = bloblist[0] #Document
+    scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+    sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    for word, score in sorted_words[:k]:
+        print("\tWord: {}, TF-IDF: {}".format(word, round(score, k)))
+    '''
+    for i, blob in enumerate(bloblist):
+        print("Top words in document {}".format(i + 1))
+        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        for word, score in sorted_words[:k]:
+            print("\tWord: {}, TF-IDF: {}".format(word, round(score, k)))
+    '''
 
 
 
@@ -36,23 +58,14 @@ document2 = """Stylish, fully remodeled home in upscale NW Alamo Heights Area.
 	 """
 
 #Cheap
-document3 = """Welcome to stay in private room with queen bed and detached private bathroom on the second floor. 
-	Another private bedroom with sofa bed is available for additional guests. 10$ for an additional guest.\n
-	10min from IAH airport\nAirport pick-up/drop off is available for $10/trip.
-
+document3 = """
 Private Room (shared bathroom) in a warm family home. It's perfect for business trips, vacation and extended stays. 
 If you are in town as a childbirth tourist, this is also a perfect place for you.
 	"""
 
 def main():
-    documentList = [document1, document2, document3]
-    bloblist = makeBlobList(documentList)
-    for i, blob in enumerate(bloblist):
-        print("Top words in document {}".format(i + 1))
-        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
-        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-        for word, score in sorted_words[:5]:
-            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+    documentList = [document2, document3]
+    extractTDIFWordFeatures(document1, documentList, -1)
 
 if __name__ == '__main__':
     main()
