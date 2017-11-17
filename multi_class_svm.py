4ebb574381c9e6d2b7554ffdac9aa281b13cdf60
Aaron Effron <aaroneffron@DN51s5qo.SUNet>
2017-11-16 17:44:48 -0800

    Comment out pystruct stuff since its not working properly

--- multi_class_svm.py
+++ multi_class_svm.py
@@ -12,15 +12,15 @@ import numpy as np
 #from sklearn.datasets import fetch_mldata
 from util import *
 from sklearn.datasets import load_digits
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
 from pystruct.models import MultiClassClf
 from pystruct.learners import (NSlackSSVM, OneSlackSSVM,
-                               SubgradientSSVM, FrankWolfeSSVM)
+                               SubgradientSSVM)
 
 #Import Data
-df = importData("../wine/winemag-data_first150k.csv",censor=True,filter=True,processDescriptions=True) 
+df = importData("winemag-data_first150k.csv",censor=True,filter=True,processDescriptions=True) 
      #be patient. It takes about 50 seconds
 
 # Settings
@@ -44,68 +44,59 @@ print(time.time() - start)
 y_train = data['class'][:num_train].as_matrix()
 y_dev = data['class'][num_train:].as_matrix()
 
-
+print(time.time() - start)
 # we add a constant 1 feature for the bias
-X_train_bias = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
-X_dev_bias = np.hstack([X_test, np.ones((X_dev.shape[0], 1))])
+print X_train.shape
+trainBias = np.ones((X_train.shape[0], 1))
+print trainBias.shape
+devBias = np.ones((X_dev.shape[0], 1))
 
+trainBias = np.ones((X_train.shape[0], 1))
+X_train_bias = np.hstack((trainBias, X_train.todense()))
 
-model = MultiClassClf(n_features=X_train_bias.shape[1], n_classes=10)
-n_slack_svm = NSlackSSVM(model, verbose=2, check_constraints=False, C=0.1,
+print X_train_bias.shape
+X_dev_bias = np.hstack((devBias, X_dev.todense()))
+
+y_train = data['class'][:num_train].as_matrix()
+y_dev = data['class'][num_train:].as_matrix()
+
+model = MultiClassClf(n_features=X_train_bias.shape[1], n_classes=24)
+n_slack_svm = NSlackSSVM(model, verbose=50, check_constraints=False, C=0.1,
                          batch_size=100, tol=1e-2)
 one_slack_svm = OneSlackSSVM(model, verbose=2, C=.10, tol=.001)
 subgradient_svm = SubgradientSSVM(model, C=0.1, learning_rate=0.000001,
                                   max_iter=1000, verbose=0)
 
-fw_bc_svm = FrankWolfeSSVM(model, C=.1, max_iter=50)
-fw_batch_svm = FrankWolfeSSVM(model, C=.1, max_iter=50, batch_mode=True)
-
 # n-slack cutting plane ssvm
-start = time()
+
+'''
 n_slack_svm.fit(X_train_bias, y_train)
 time_n_slack_svm = time() - start
-y_pred = np.hstack(n_slack_svm.predict(X_test_bias))
+y_pred = np.hstack((n_slack_svm.predict(X_dev_bias)))
 print("Score with pystruct n-slack ssvm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_test), time_n_slack_svm))
+      % (np.mean(y_pred == y_dev), time_n_slack_svm))
 
 ## 1-slack cutting plane ssvm
 start = time()
 one_slack_svm.fit(X_train_bias, y_train)
 time_one_slack_svm = time() - start
-y_pred = np.hstack(one_slack_svm.predict(X_test_bias))
+y_pred = np.hstack((one_slack_svm.predict(X_dev_bias)))
 print("Score with pystruct 1-slack ssvm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_test), time_one_slack_svm))
+      % (np.mean(y_pred == y_dev), time_one_slack_svm))
 
 #online subgradient ssvm
 start = time()
 subgradient_svm.fit(X_train_bias, y_train)
 time_subgradient_svm = time() - start
-y_pred = np.hstack(subgradient_svm.predict(X_test_bias))
+y_pred = np.hstack((subgradient_svm.predict(X_dev_bias)))
 
 print("Score with pystruct subgradient ssvm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_test), time_subgradient_svm))
+      % (np.mean(y_pred == y_dev), time_subgradient_svm))
 
 # the standard one-vs-rest multi-class would probably be as good and faster
 # but solving a different model
-libsvm = LinearSVC(multi_class='crammer_singer', C=.1)
-start = time()
-libsvm.fit(X_train, y_train)
-time_libsvm = time() - start
-print("Score with sklearn and libsvm: %f (took %f seconds)"
-      % (libsvm.score(X_test, y_test), time_libsvm))
-
-
-start = time()
-fw_bc_svm.fit(X_train_bias, y_train)
-y_pred = np.hstack(fw_bc_svm.predict(X_test_bias))
-time_fw_bc_svm = time() - start
-print("Score with pystruct frankwolfe block coordinate ssvm: %f (took %f seconds)" %
-      (np.mean(y_pred == y_test), time_fw_bc_svm))
-
-start = time()
-fw_batch_svm.fit(X_train_bias, y_train)
-y_pred = np.hstack(fw_batch_svm.predict(X_test_bias))
-time_fw_batch_svm = time() - start
-print("Score with pystruct frankwolfe batch ssvm: %f (took %f seconds)" %
-      (np.mean(y_pred == y_test), time_fw_batch_svm))
 '''
+libsvm = LinearSVC(multi_class='crammer_singer', C=.1)
+libsvm.fit(X_train_bias, y_train)
+print("Score with sklearn and libsvm: %f (took %f seconds)"
+      % (libsvm.score(X_dev_bias, y_dev), 2))
