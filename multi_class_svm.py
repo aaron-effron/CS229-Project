4a8f7b0e42cf263050493343ff8fa0b8c967bd07
Aaron Effron <aaroneffron@DN51vs9s.SUNet>
2017-12-14 18:48:27 -0800

    Code cleanup and new implementation in multiclass

--- multi_class_svm.py
+++ multi_class_svm.py
@@ -15,116 +15,51 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
-from pystruct.models import MultiClassClf
-from pystruct.learners import (NSlackSSVM, OneSlackSSVM,
-                               SubgradientSSVM)
-
-#Import Data
-df = importData("winemag-data_first150k.csv",censor=True,filter=True,processDescriptions=True) 
-     #be patient. It takes about 50 seconds
-
 # Settings
-num_examples = 25000
-#num_examples = 50000
-perc_train   = 0.70
-#featurizer   = extractWordFeatures(1)
+#------------------------------------------------------------------------------------------------------
+task = 2 #1 = Red vs. White, 2 = Variety
 featurizer   = extractCharFeatures(5)
   #Other options: genericExtractor  extractWordFeatures(1)  extractCharFeatures(5)
   #               featureExtractor([extractWordFeatures(1),extractWordFeatures(2)])
+  #               extracWord2VecFeatures(trainWord2Vec(min_count=2,size=400,window=4))
+balanceDataset = True
 
-# Subset Data
-data = df.iloc[0:num_examples,:]
-num_train = int(num_examples*perc_train)
+data_test = pd.read_csv('data-processed/data.test')
+data_dev = pd.read_csv('data-processed/data.dev')
+data_train = pd.read_csv('data-processed/data.train')
 
 import time
 start = time.time()
-X_train, X_dev, feature_names = DesignMatrix(data=data['description'].as_matrix(),
-                                        featurizer=featurizer,
-                                        num_train=num_train)
+X_train,X_dev,X_test,feature_names = DesignMatrix(data_train=data_train['description'].as_matrix(),
+                                                   data_dev=data_dev['description'].as_matrix(),
+                                                   data_test=data_test['description'].as_matrix(),
+                                                   featurizer=featurizer)
 print(time.time() - start)
 
-y_train = data['class'][:num_train].as_matrix()
-y_dev = data['class'][num_train:].as_matrix()
-
-print(time.time() - start)
 # we add a constant 1 feature for the bias
-print X_train.shape
-trainBias = np.ones((X_train.shape[0], 1))
-print trainBias.shape
+
 devBias = np.ones((X_dev.shape[0], 1))
+testBias = np.ones((X_test.shape[0], 1))
 
 trainBias = np.ones((X_train.shape[0], 1))
 X_train_bias = np.hstack((trainBias, X_train.todense()))
-
-print X_train_bias.shape
 X_dev_bias = np.hstack((devBias, X_dev.todense()))
+X_test_bias = np.hstack((testBias, X_test.todense()))
 
-y_train = data['class'][:num_train].as_matrix()
-y_dev = data['class'][num_train:].as_matrix()
-
-model = MultiClassClf(n_features=X_train_bias.shape[1], n_classes=24)
-n_slack_svm = NSlackSSVM(model, verbose=50, check_constraints=False, C=0.1,
-                         batch_size=100, tol=1e-2)
-one_slack_svm = OneSlackSSVM(model, verbose=2, C=.10, tol=.001)
-subgradient_svm = SubgradientSSVM(model, C=0.1, learning_rate=0.000001,
-                                  max_iter=1000, verbose=0)
-
-# n-slack cutting plane ssvm
-
-'''
-n_slack_svm.fit(X_train_bias, y_train)
-time_n_slack_svm = time() - start
-y_pred = np.hstack((n_slack_svm.predict(X_dev_bias)))
-print("Score with pystruct n-slack ssvm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_dev), time_n_slack_svm))
-
-## 1-slack cutting plane ssvm
-start = time()
-one_slack_svm.fit(X_train_bias, y_train)
-time_one_slack_svm = time() - start
-y_pred = np.hstack((one_slack_svm.predict(X_dev_bias)))
-print("Score with pystruct 1-slack ssvm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_dev), time_one_slack_svm))
-
-#online subgradient ssvm
-start = time()
-subgradient_svm.fit(X_train_bias, y_train)
-time_subgradient_svm = time() - start
-y_pred = np.hstack((subgradient_svm.predict(X_dev_bias)))
-
-print("Score with pystruct subgradient ssvm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_dev), time_subgradient_svm))
-
-# the standard one-vs-rest multi-class would probably be as good and faster
-# but solving a different model
-
-Train Score with sklearn and libsvm: 0.935657 (took 2.000000 seconds)
-Dev Score with sklearn and libsvm: 0.655067
-
-
-libsvm = LinearSVC(multi_class='crammer_singer', C=.1)
-libsvm.fit(X_train_bias, y_train)
-print("Train Score with sklearn and libsvm: %f"
-      % (libsvm.score(X_train_bias, y_train)))
-print("Dev Score with sklearn and libsvm: %f"
-      % (libsvm.score(X_dev_bias, y_dev)))
-
-coef = libsvm.coef_[0,:] #only gets the theta vector for first class in multiclass prediction
-                        #use indiced other than 0 to get theta vector for other classes
-
-weights = pd.DataFrame({'feature': feature_names,'coef': coef})
-weights.sort_values(by='coef',inplace=True,ascending=False)
-outputWeights('weights.txt',weights,featureWidth=50)
-
-
-#Binary
-
-Train Score with sklearn and libsvm: 0.997086
-Dev Score with sklearn and libsvm: 0.976800
-'''
-
-y_train_bin = (1*(data['color'][:num_train] == 'Red')).as_matrix()
-y_dev_bin   = (1*(data['color'][num_train:] == 'Red')).as_matrix()
+if task == 1:
+    labels = ['White','Red']
+    cnf_plt_vals = True
+    y_test  = (1*(data_test['color'] == 'Red')).as_matrix()
+    y_dev   = (1*(data_dev['color'] == 'Red')).as_matrix()
+    y_train = (1*(data_train['color'] == 'Red')).as_matrix()
+elif task ==2:
+    labels = CLASS_MAP
+    cnf_plt_vals = False
+    y_test  = data_test['class'].as_matrix()
+    y_dev   = data_dev['class'].as_matrix()
+    y_train = data_train['class'].as_matrix()
+else:
+    assert False
 
 libsvm = LinearSVC(multi_class='ovr', C=0.01)
 libsvm.fit(X_train_bias, y_train)
@@ -132,4 +67,5 @@ print("Train Score with sklearn and libsvm: %f"
       % (libsvm.score(X_train_bias, y_train)))
 print("Dev Score with sklearn and libsvm: %f"
       % (libsvm.score(X_dev_bias, y_dev)))
-
+print("Test Score with sklearn and libsvm: %f"
+      % (libsvm.score(X_test_bias, y_test)))
